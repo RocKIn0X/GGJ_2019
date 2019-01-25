640aadb38191b9b45a2c096ca6571c27a904a53c
hanbunne <hanbunne@gmail.com>
2019-01-26 03:17:11 +0700

    Add Menu Scene

---
+++ Assets/Scripts/Scene_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //--------------------------------Load -------------------------------
+
+    public void loadNewGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void loadCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void loadHowTo()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
