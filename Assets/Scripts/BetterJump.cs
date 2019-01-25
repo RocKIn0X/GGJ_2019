fea6f1cce1981ff3ef6da63c255fc9f83e89d116
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2019-01-26 00:43:00 +0700

    Improve jump feeling

---
+++ Assets/Scripts/BetterJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour
+{
+    private float normalGravityScale;
+    [SerializeField]
+    private float fallGravityScale;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        normalGravityScale = rb.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallGravityScale;
+        }
+        else
+        {
+            rb.gravityScale = normalGravityScale;
+        }
+    }
+}
