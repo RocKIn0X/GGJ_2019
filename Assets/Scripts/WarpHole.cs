51ba520fcbc9fb28bb2541a581a2d93f33d98790
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2019-01-26 14:56:53 +0700

    Add warp hole for dog

---
+++ Assets/Scripts/WarpHole.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpHole : MonoBehaviour
+{
+    public Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GetDestination ()
+    {
+        return destination.position;
+    }
+}
