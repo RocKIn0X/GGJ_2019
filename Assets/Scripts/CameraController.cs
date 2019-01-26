6e291c2f4b1f7022e2e3377fda2a17227639d6d8
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2019-01-26 21:11:37 +0700

    Add new follow camera and map prefabs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,12 +7,42 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public float smoothTime = 0.3f;
     public Vector3 offset;
+    [SerializeField]
+    private float minX;
+    [SerializeField]
+    private float maxX;
+    [SerializeField]
+    private float minY;
+    [SerializeField]
+    private float maxY;
+    [SerializeField]
+    private BoxCollider2D leftBorder;
+    [SerializeField]
+    private BoxCollider2D rightBorder;
     private Vector3 velocity = Vector3.zero;
+    private bool playerOutOfBox;
+
+    void Start()
+    {
+        playerOutOfBox = false;
+        minX = transform.position.x;
+        minY = transform.position.y;
+        maxY = transform.position.y + 3f;
+    }
 
     void FixedUpdate()
     {
-        Vector3 desiredPosition = new Vector3(target.position.x, target.position.y + offset.y, transform.position.z) + offset;
-        Vector3 SmoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
-        transform.position = SmoothPosition;
+        if (playerOutOfBox)
+        {
+            Vector3 desiredPosition = new Vector3(Mathf.Clamp(target.position.x, minX, maxX), transform.position.y, transform.position.z);
+            Vector3 SmoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
+            transform.position = SmoothPosition;
+        }
+    }
+
+    public void SetIsPlayerOutOfBorder(bool b)
+    {
+        playerOutOfBox = b;
+        offset = transform.position - target.position;
     }
 }
